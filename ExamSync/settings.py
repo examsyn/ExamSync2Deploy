208822f4259d5787730105b2115aadc6953956bb
examsyn <examsync.ustp@gmail.com>
2024-12-14 14:01:37 +0800

    Update settings.py

--- ExamSync/settings.py
+++ ExamSync/settings.py
@@ -143,9 +143,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "UserManagement/Static",  # This tells Django where to find your static files
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
