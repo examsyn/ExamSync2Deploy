83eb5fe0afccf29433db8d46e5fbf04734f1bd9e
examsyn <examsync.ustp@gmail.com>
2024-12-14 20:58:49 +0800

    Update settings.py

--- ExamSync/settings.py
+++ ExamSync/settings.py
@@ -12,22 +12,25 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8zlws$mgufv@(fj9yrluoj!jx+(0*c!f(%!d6a^7tn5c73y_=y'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'examsync-do-user-14266440-0.h.db.ondigitalocean.com', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
 
 SESSION_COOKIE_AGE = 1209600  # 2 weeks
@@ -39,11 +42,11 @@ SESSION_SAVE_EVERY_REQUEST = True
 
 # settings.py
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # You can use other SMTP services like SendGrid, Mailgun, etc.
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'examsync.ustp@gmail.com'  # Replace with your email
-EMAIL_HOST_PASSWORD = 'okzi rllh okys wdqy'  # Replace with your email password (or App password for Gmail)
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')  # Defaults to Gmail if not set
+EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # Ensure this is set
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') 
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER  # Optional: Set a default sender email for your app
 
@@ -97,15 +100,13 @@ WSGI_APPLICATION = 'ExamSync.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'examsync',
-        'USER': 'doadmin',
-        'PASSWORD': 'AVNS_t69IAsvdRDKw70wQdB1',
-        'HOST': 'examsync-do-user-14266440-0.h.db.ondigitalocean.com',
-        'PORT': '25060',
+        'NAME': os.getenv('DB_NAME', 'examsync'),
+        'USER': os.getenv('DB_USER', 'doadmin'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -143,6 +144,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "UserManagement/Static",  # This tells Django where to find your static files
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
